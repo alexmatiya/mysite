8c0398b9d0c45897679d6f621366c14d0ed972f2
Alex Matiya <matiacka@yandex.ru>
2023-11-02 14:57:24 +0300

    добавил конкретно прикладной менеджер published

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title', )}  # слаг формируется сам при заполнении тайтла
+    raw_id_fields = ['author']  # при создании поста автор теперь с поисковом окне а не в выпадающем
+    date_hierarchy = 'publish'  # добавилась навигационная ссылка по датам
+    ordering = ['status', 'publish']
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,31 +1,39 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+# наш менеджер, который возращает только опубликованные посты
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
+
+class Post(models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    objects = models.Manager()  # менеджер по умолчанию
+    published = PublishedManager()  # менеджер кокретно-прикладной
 
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
 
     def __str__(self):
