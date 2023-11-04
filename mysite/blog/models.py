00fe65debbd509ab8ab0c3bf8b839537b5461176
Alex Matiya <matiacka@yandex.ru>
2023-11-04 09:16:24 +0300

    добавил пагинацию, шаре с отправкой на почту

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # наш менеджер, который возращает только опубликованные посты
@@ -11,21 +12,29 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
+
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2, choices=Status.choices, default=Status.DRAFT
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
     )
     objects = models.Manager()  # менеджер по умолчанию
     published = PublishedManager()  # менеджер кокретно-прикладной
@@ -38,3 +47,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
